5275a81cda759fc4d9b34d25e3a24318880569b7
Damian Łoziński <lozinski.d@gmail.com>
2018-12-07 16:44:07 +0100

    Installaction scipt added

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+echo '
+# Git prompt config
+GIT_PROMPT_ONLY_IN_REPO=1
+source ~/.bash-git-prompt/gitprompt.sh' >> ~/.bashrc
+
+
+git config --global pull.rebase true
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.k '!gitk --all'
+
